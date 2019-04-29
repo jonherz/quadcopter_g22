2a4404c01e45c92e336c2e46fe597f8ad81a043b
GustavRosin <49003886+GustavRosin@users.noreply.github.com>
2019-04-29 12:11:32 +0200

    LQR
    Tried and faild at LQR

--- crazyflie-firmware/simulink-model/LQR.m
+++ crazyflie-firmware/simulink-model/LQR.m
@@ -4,7 +4,16 @@ clc
 Q = eye(8);
 R = eye(4);
 load('lqr.mat');
-A = lqr.A;
-B = lqr.B;
+T = 0.01;
+Ac = lqr.A;
+Bc = lqr.B;
+Cc = lqr.C;
+SYS = ss(Ac,Bc,Cc,0);
+KUNG = c2d(SYS,T);
+
+Ad = KUNG.A;
+Bd = KUNG.B;
+Cd = KUNG.C;
+Dd = KUNG.D;
 clear lqr;
-[K,S,e] = lqr(A,B,Q,R);
+[KLQR,S,e] = dlqr(KUNG.A,KUNG.B,Q,R);
